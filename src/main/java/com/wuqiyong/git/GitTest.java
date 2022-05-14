bb91f2f84906620fb69ef6b05dcd5406405c4cf7
wuqiyong <105497916+wuqiyong@users.noreply.github.com>
2022-05-14 18:26:50 +0800

    pull commit

--- src/main/java/com/wuqiyong/git/GitTest.java
+++ src/main/java/com/wuqiyong/git/GitTest.java
@@ -7,6 +7,6 @@ public class GitTest {
         System.out.println("hot-fix");
         System.out.println("hot-fix test");
         System.out.println("master test");
-        System.out.println("pull test");
+        System.out.println("pull-test");
     }
 }
