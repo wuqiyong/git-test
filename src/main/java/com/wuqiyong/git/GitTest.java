991b55c891b19ead5806f22bea349875efeb8e98
wuqiyong <252358599@qq.com>
2022-05-14 17:54:50 +0800

    versions2.0 hot-fix

--- src/main/java/com/wuqiyong/git/GitTest.java
+++ src/main/java/com/wuqiyong/git/GitTest.java
@@ -4,5 +4,6 @@ package com.wuqiyong.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("versions2.0");
+        System.out.println("hot-fix");
     }
 }
