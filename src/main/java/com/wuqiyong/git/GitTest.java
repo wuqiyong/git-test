0ad4cd836a468ef2c9942254348e7f123a352097
wuqiyong <252358599@qq.com>
2022-05-14 18:16:25 +0800

    push

--- src/main/java/com/wuqiyong/git/GitTest.java
+++ src/main/java/com/wuqiyong/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hot-fix");
         System.out.println("hot-fix test");
         System.out.println("master test");
+        System.out.println("push");
     }
 }
